2b9462b68b868d4e5f92d10f9271fe026d051a56
Marc Brooks <mbrooks@mitre.org>
2020-11-16 18:58:33 -0500

    Added a testapp update that includes a staticfile.

--- testapp/testapp.py
+++ testapp/testapp.py
@@ -9,17 +9,17 @@ import fastapi_jinja
 app = FastAPI()
 
 folder = os.path.dirname(__file__)
-print(f"folder = {folder}")
 template_folder = os.path.join(folder, "templates")
 print(template_folder)
-template_folder = os.path.abspath(template_folder)
+static_folder = os.path.join(template_folder, "static")
+print(static_folder)
 
-
+app.mount("/static", StaticFiles(directory=static_folder), name="static")
 fastapi_jinja.global_init(template_folder)
 
 
 @app.get("/")
-@fastapi_jinja.template("test.j2")
+@fastapi_jinja.template("test.html")
 async def root(request: Request):
   render_dict = {
     "title": "Test Title",
